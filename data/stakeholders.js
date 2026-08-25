33c2137616f5c9feeb6964e58643b44fed737e58
BAboubakr <aboubakre.benazzouz@gmail.com>
2026-08-25 18:11:20 +0200

    Add separate stakeholder data layer

---
+++ data/stakeholders.js
@@ -0,0 +1,8 @@
+export const stakeholders = [
+  ['MASEN','PUBLIC DEVELOPER','Solar, storage & hydrogen programmes','6 linked developments'],
+  ['ONEE','UTILITY / GRID OPERATOR','Transmission, generation & procurement','8 linked developments'],
+  ['OCP Group','INDUSTRIAL OFFTAKER','Green ammonia & industrial decarbonisation','4 linked developments'],
+  ['AMMC','REGULATOR','Capital markets and investment framework','2 linked developments'],
+  ['Nareva','DEVELOPER','Wind, solar and infrastructure','5 linked developments'],
+  ['Port of Tanger Med','PORT / LOGISTICS','Hydrogen and export infrastructure','3 linked developments']
+];

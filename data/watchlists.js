84263006750cffa9d9e1d9b5ca2229f0932a0741
BAboubakr <aboubakre.benazzouz@gmail.com>
2026-08-25 18:16:34 +0200

    Add monitored Morocco source and competitor watchlists

---
+++ data/watchlists.js
@@ -0,0 +1,19 @@
+export const watchlists = {
+  marketSources: [
+    {name:'MASEN e-Tendering',url:'https://etendering.masen.ma/',type:'Official procurement',priority:'HIGH'},
+    {name:'ANRE',url:'https://anre.ma/',type:'Regulation & grid capacity',priority:'HIGH'},
+    {name:'ONEE',url:'https://www.onee.ma/',type:'Utility & grid',priority:'HIGH'},
+    {name:'Ministry of Energy Transition',url:'https://www.mtedd.gov.ma/',type:'Policy',priority:'HIGH'},
+    {name:'OCP Group',url:'https://www.ocpgroup.ma/',type:'Industrial / offtake',priority:'MEDIUM'}
+  ],
+  competitorSources: [
+    {name:'AFRY',url:'https://afry.com/',priority:'HIGH'},
+    {name:'Artelia',url:'https://www.arteliagroup.com/',priority:'MEDIUM'},
+    {name:'Tractebel',url:'https://tractebel-engie.com/',priority:'HIGH'},
+    {name:'Mott MacDonald',url:'https://www.mottmac.com/',priority:'MEDIUM'},
+    {name:'WSP',url:'https://www.wsp.com/',priority:'MEDIUM'},
+    {name:'Worley',url:'https://www.worley.com/',priority:'HIGH'},
+    {name:'Egis',url:'https://www.egis-group.com/',priority:'LOW'},
+    {name:'ILF Consulting Engineers',url:'https://www.ilf.com/',priority:'HIGH'}
+  ]
+};

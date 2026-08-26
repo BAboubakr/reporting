231a34e07d92205eca60592fcbed9dd986b5178c
Atlas Monitor <41898282+github-actions[bot]@users.noreply.github.com>
2026-08-26 19:20:43 +0000

    chore: update Morocco intelligence signals

--- data/signals.js
+++ data/signals.js
@@ -8,7 +8,7 @@ export const signals = [
     "source": "fundsforNGOs News",
     "sourceType": "news",
     "published": "2026-08-20T08:56:26+00:00",
-    "detected": "2026-08-26T17:27:29.314178+00:00",
+    "detected": "2026-08-26T19:20:43.693009+00:00",
     "categories": [
       "Investment"
     ],

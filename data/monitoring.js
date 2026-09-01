c6985fe129bb837c805ae29d97e75ffbe7095a50
BAboubakr <aboubakre.benazzouz@gmail.com>
2026-09-01 13:33:19 +0200

    Add JESA to continuous competitor monitoring

--- data/monitoring.js
+++ data/monitoring.js
@@ -10,17 +10,11 @@ export const monitoring = {
     'Morocco renewable energy manufacturing investment'
   ],
   competitorQueries: [
-    'AFRY Morocco renewable energy',
-    'Artelia Morocco renewable energy',
-    'Tractebel Morocco energy renewable hydrogen',
-    'Mott MacDonald Morocco energy renewable',
-    'WSP Morocco energy renewable',
-    'Worley Morocco hydrogen OCP energy',
-    'Egis Morocco energy renewable',
-    'ILF Consulting Engineers Morocco hydrogen energy',
-    'DNV Morocco renewable energy grid hydrogen',
-    'NOVEC Morocco energy renewable grid hydro',
-    'INGEMA Morocco energy engineering renewable'
+    'AFRY Morocco renewable energy','Artelia Morocco renewable energy','Tractebel Morocco energy renewable hydrogen',
+    'Mott MacDonald Morocco energy renewable','WSP Morocco energy renewable','Worley Morocco hydrogen OCP energy',
+    'Egis Morocco energy renewable','ILF Consulting Engineers Morocco hydrogen energy','DNV Morocco renewable energy grid hydrogen',
+    'NOVEC Morocco energy renewable grid hydro','INGEMA Morocco energy engineering renewable',
+    'JESA Morocco renewable energy solar OCP','JESA Morocco engineering energy infrastructure','JESA OCP solar project'
   ],
   officialPages: [
     {name:'ONEE tenders',url:'https://www.one.org.ma/FR/pages/aoselect.asp?action=1&domaine=&esp=2&id1=7&id2=64&id3=54&nao=&nature=&objet=&page=1&t1=&t2=&t3=1&type=',type:'official-tender'},
